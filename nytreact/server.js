0be52f45a532fa3a632899be62dd894f42bb2687
Christopher Chen <christopher.chen1124@gmail.com>
2017-10-23 14:41:37 -0700

    Commit

--- nytreact/server.js
+++ nytreact/server.js
@@ -26,6 +26,28 @@ mongoose.connect(
   }
 );
 
+/////////////////
+var databaseUrl='mongodb://localhost/week18day3mongoose';
+
+if(process.env.MONGODB_URI)
+{
+	mongoose.connect(process.env.MONGODB_URI);
+}
+else
+{
+	mongoose.connect(databaseUrl);
+}
+var db=mongoose.connection;
+
+db.on('error',function(err){
+	console.log('Mongoose Error: ',err);
+});
+
+db.once('open',function(){
+	console.log('Mongoose connection successful');
+});
+/////////////////
+
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
